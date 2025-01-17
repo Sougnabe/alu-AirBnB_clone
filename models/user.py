30201e0bc0c4cc2926b4af748f597a9bb0f2ff07
TAWE KELVIN <169045295+kelvintawe12@users.noreply.github.com>
2025-01-17 21:09:39 +0200

    handeling merges

--- models/user.py
+++ models/user.py
@@ -1,42 +1,22 @@
-'''module importation'''
+#!/usr/bin/python3
+"""
+User Module
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''creation of the classe'''
+    """User class inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email')
-        self.password = kwargs.get('password')
-        self.first_name = kwargs.get('first_name')
-        self.last_name = kwargs.get('last_name')
+    def create(self):
+        """Creates a new user instance"""
+        # Replace save_to_db with actual save logic
         self.save()
 
     def save(self):
-        self.create()
-        return True
-
-    def update(self, **kwargs):
-        '''update function'''
-        self.update()
-        return True
-
-    def delete(self):
-        '''delete fonction'''
-        self.delete()
-        return True
-
-    def create(self):
-        '''create function'''
-        self.save_to_db()
-        return True
-
-    def update(self):
-        '''update'''
-        self.save_to_db()
-        return True
+        """Saves the user instance"""
+        super().save()  # Ensure BaseModel's save logic is used
